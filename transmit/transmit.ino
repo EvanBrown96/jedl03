dfcc8f66201e2821d9b1918015c44842b59f82e3
unknown <evanbrown@ku.edu>
2019-11-11 21:42:47 -0600

    removed commented out code

--- transmit/transmit.ino
+++ transmit/transmit.ino
@@ -18,56 +18,35 @@ bool addBit(int new_bit) {
   }
 }
 
-//int getBinaryIntFromInt(int int_source, int position)
-//{
-//  int mask = pow(2, position);
-//  int result = int_source & mask;
-//  if (result == mask) return 1;
-//  else return 0;
-//}
-
 void addChar(char new_char) {
+  /**
+   * add character to the buffer
+   */
   for(int i = 0; i < 8; i++) {
     addBit(new_char%2);
     new_char >>= 1;
   }
-//  addBit(getBinaryIntFromInt(new_char, 0));
-//  addBit(getBinaryIntFromInt(new_char, 1));
-//  addBit(getBinaryIntFromInt(new_char, 2));
-//  addBit(getBinaryIntFromInt(new_char, 3));
-//  addBit(getBinaryIntFromInt(new_char, 4));
-//  addBit(getBinaryIntFromInt(new_char, 5));
-//  addBit(getBinaryIntFromInt(new_char, 6));
-//  addBit(getBinaryIntFromInt(new_char, 7));
 }
 
 void addString(String new_string) {
+  /**
+   * add a string to the buffer
+   */
   for (int i = 0; i < new_string.length(); i++) {
     addChar(new_string[i]);
   }
 }
 
-//void transmitBit(int bit_to_transmit)
-//{
-//  //Transmit a bit, in whatever way you want.
-//  
-//}
-//
-//void transmitBits()
-//{
-//  bit_position = 0; //Make sure that we start from the beggining.
-//  while (bit_position != max_bit_position)
-//  {
-//    transmitBit(bit_buffer[bit_position]);
-//    bit_position++;
-//  }
-//  max_bit_position = 0; //When we are done, we set the max back to zero, thereby clearing the buffer.
-//}
-
+// tracks what bit in the buffer is being transmitted
 int transmit_pos = 0;
 
 void transmitBit(){
+  /**
+   * periodic transmission attempt on the bit buffer
+   */
   if(PHY::update(bit_buffer[transmit_pos])){
+    // attempt to update the current bit being transmitted
+    // move onto next bit if successful
     transmit_pos++;
     if(transmit_pos == saved_bits){
       transmit_pos = 0;
